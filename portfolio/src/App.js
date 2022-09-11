7c5fdbbd54bd7a16b8195028e5ed33bf3da2988d
noyflaysher <noyflaysher12@gmail.com>
2022-09-11 16:00:34 +0300

    technologies section

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,6 +3,7 @@ import Particles from "./Particles";
 import NavigationBar from "./screens/navigationBar/NavigationBar";
 import Header from "./screens/header/Header";
 import AboutMe from "./screens/aboutMe/AboutMe";
+import Technologies from "./screens/technologies/Technologies";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <Particles />
       <AboutMe />
+      <Technologies />
     </div>
   );
 }
---
+++ portfolio/src/helpers/techs.js
@@ -0,0 +1,50 @@
+export const techs = [
+  {
+    name: "HTML 5",
+    iconName: "html5",
+  },
+  {
+    name: "CSS3",
+    iconName: "css3",
+  },
+  {
+    name: "SASS",
+    iconName: "sass",
+  },
+
+  {
+    name: "Javascript",
+    iconName: "javascript",
+  },
+
+  {
+    name: "React",
+    iconName: "react",
+  },
+
+  {
+    name: "Mongo DB",
+    iconName: "mongodb",
+  },
+  {
+    name: "NodeJS",
+    iconName: "nodejs",
+  },
+  {
+    name: "Bootstrap",
+    iconName: "bootstrap",
+  },
+  {
+    name: "Java",
+    iconName: "java",
+  },
+
+  {
+    name: "Git",
+    iconName: "git",
+  },
+  {
+    name: "Linux",
+    iconName: "linux",
+  },
+];
