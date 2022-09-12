dccbfbc60848773b65d91a903e22741ec51c831a
noyflaysher <noyflaysher12@gmail.com>
2022-09-12 15:50:01 +0300

    send email to gmail

--- portfolio/src/screens/contactMe/ContactMe.js
+++ portfolio/src/screens/contactMe/ContactMe.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useRef } from "react";
+import emailjs from "emailjs-com";
 import "./contactMe.css";
 import { navigationLinks } from "../../helpers/navigationLinks";
 import TextField from "@mui/material/TextField";
@@ -8,6 +9,8 @@ import AccountCircle from "@mui/icons-material/AccountCircle";
 import EmailIcon from "@mui/icons-material/Email";
 import EditIcon from "@mui/icons-material/Edit";
 import PersonIcon from "@mui/icons-material/Person";
+import SendIcon from "@mui/icons-material/Send";
+import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 
 function createLinks() {
   return navigationLinks.map((e, idx) => (
@@ -18,11 +21,76 @@ function createLinks() {
 }
 
 function ContactMe() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [emailError, setEmailError] = useState();
+  const [nameError, setNameError] = useState();
+  const [messageError, setMessageError] = useState();
+  const [formIsValid, setFormIsValid] = useState();
+  const form = useRef();
+
+  let result = 0;
+
+  const changeEmailHandler = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const changeNameHandler = (event) => {
+    setName(event.target.value);
+  };
+
+  const changeMessageHandler = (event) => {
+    setMessage(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    if (name.length > 0) {
+      setNameError(false);
+      result++;
+    } else setNameError(true);
+
+    if (/^\S+@\S+\.\S+$/.test(email)) {
+      setEmailError(false);
+      result++;
+    } else setEmailError(true);
+
+    if (message.length > 0) {
+      setMessageError(false);
+      result++;
+    } else setMessageError(true);
+
+    if (result === 3) sendEmail(event);
+
+    setName("");
+    setEmail("");
+    setMessage("");
+  };
+
+  const sendEmail = (e) => {
+    console.log("send");
+
+    e.preventDefault();
+
+    emailjs
+      .sendForm("gmail", "template_5eb8f7e", form.current, "T_nEY98BzJbsxVeUC")
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
     <div className="contactMe__container" id="contact">
       <h1 className="contactMe__headline">CONTACT ME</h1>
       <div>
-        <form>
+        <form ref={form}>
           <div className="contactMe__input">
             <TextField
               variant="outlined"
@@ -34,12 +102,14 @@ function ContactMe() {
                 ),
               }}
               label="Full Name"
+              onChange={changeNameHandler}
               name="name"
+              value={name}
+              error={nameError}
               fullWidth
               sx={{
                 "& > :not(style)": {
                   m: 1,
-                  //   width: "500px",
                   color: "black",
                   fontFamily: "inherit",
                   fontSize: "1.1rem",
@@ -60,11 +130,13 @@ function ContactMe() {
               }}
               label="Email"
               name="email"
+              error={emailError}
+              value={email}
+              onChange={changeEmailHandler}
               fullWidth
               sx={{
                 "& > :not(style)": {
                   m: 1,
-                  //   width: "500px",
                   color: "black",
                   fontFamily: "inherit",
                   fontSize: "1.1rem",
@@ -85,13 +157,15 @@ function ContactMe() {
               variant="outlined"
               label="Message"
               name="message"
+              error={messageError}
+              value={message}
+              onChange={changeMessageHandler}
               multiline
               fullWidth
               row={10}
               sx={{
                 "& > :not(style)": {
                   m: 1,
-                  //   width: "500px",
                   height: "200px",
                   color: "black",
                   fontFamily: "inherit",
@@ -100,10 +174,31 @@ function ContactMe() {
               }}
             />
           </div>
-
-          <button className="contactMe__button" type="submit">
+          <Button
+            onClick={submitHandler}
+            type="submit"
+            size="md"
+            variant="outlined"
+            sx={{
+              mt: 1.7,
+              mb: 2,
+              width: "90px",
+              fontWeight: "bold",
+              border: "3px",
+              fontSize: "1.1rem",
+            }}
+            endIcon={<KeyboardArrowRightIcon />}
+          >
+            Send
+          </Button>
+          {/* <button
+            // disabled={!formIsValid}
+            onClick={submitHandler}
+            className="contactMe__button"
+            type="submit"
+          >
             Submit
-          </button>
+          </button> */}
         </form>
       </div>
     </div>
